81fdd6e9f48f01255c17619a8a0a7f7a8ebc7bf7
vandanaJha-098 <vandanajha987321@gmail.com>
2026-01-03 13:12:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HelmetProvider } from 'react-helmet-async';
+import { Helmet } from 'react-helmet';
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
+    <Helmet>
       <App />
-    </HelmetProvider>
+    </Helmet>
   </React.StrictMode>
 );
 
